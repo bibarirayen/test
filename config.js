84d940ec6d2902d6ed1dd655fd3f025d0930f7be
bibarirayen <150616884+bibarirayen@users.noreply.github.com>
2025-06-15 14:03:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // L instance elli bech tconnecti m3a lbd
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('sql7784876', 'sql7784876', '9zlLsr1Bel', {
-  host: 'sql7.freesqldatabase.com	',        
+  host: 'sql7.freesqldatabase.com',        
   dialect: 'mysql',         
   logging: false,          
   define: {
